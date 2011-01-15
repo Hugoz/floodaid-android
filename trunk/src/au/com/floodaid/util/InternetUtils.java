fca7c5489a0fdc3f69f9f25868c49f5e46281596
ronaldnooij <ronaldnooij@d8924caf-5d75-acf0-e6e7-bc3c6e4dadb3>
2011-01-15 22:14:27 +0000

    added api call stuff

--- trunk/src/au/com/floodaid/util/InternetUtils.java
+++ trunk/src/au/com/floodaid/util/InternetUtils.java
@@ -1,10 +1,15 @@
 package au.com.floodaid.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 import android.graphics.drawable.Drawable;
 import android.util.Log;
+
+import org.json.JSONObject;
 
 /**
  * Class with utility methods used for connected apps 
@@ -37,4 +42,55 @@ public class InternetUtils {
 			return null;
 		}
 	}
+	
+	/**
+	 * Execute API call
+	 * This is not creating a new thread and therefore it will freeze the main thread until JSONobject has been retrieved.
+	 *  
+	 * @param url
+	 * @return JSONObject
+	 */
+	public static JSONObject executeApiCall(String url) {
+		try {
+			Log.d(TAG, "Execute API call: " + url);
+			URL urlObj = new URL(url);
+			InputStream is = (InputStream) urlObj.getContent();
+			String result = convertStreamToString(is);
+			JSONObject json=new JSONObject(result);
+			Log.d(TAG, "Executed API call " + urlObj.getFile()); 
+			return json;
+			
+		} catch (Exception e) {
+			Log.e(TAG, "Unable to execute API call " + url, e);
+			return null;
+		}
+	}
+	
+	/**
+    *
+    * @param is
+    * @return String
+    */
+   public static String convertStreamToString(InputStream is) {
+       BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+       StringBuilder sb = new StringBuilder();
+
+       String line = null;
+       try {
+           while ((line = reader.readLine()) != null) {
+               sb.append(line + "\n");
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       } finally {
+           try {
+               is.close();
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+       }
+       return sb.toString();
+   }
+
+
 }
