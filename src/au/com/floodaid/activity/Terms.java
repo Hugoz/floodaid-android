f5d6e638bbb74d90e2cf4e4a12d17dd53593d28e
ronaldnooij <ronaldnooij@d8924caf-5d75-acf0-e6e7-bc3c6e4dadb3>
2011-01-16 22:27:30 +0000

    Load terms using background threading

--- src/au/com/floodaid/activity/Terms.java
+++ src/au/com/floodaid/activity/Terms.java
@@ -4,6 +4,7 @@ import org.json.JSONObject;
 import au.com.floodaid.util.InternetUtils;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.TextView;
 import au.com.floodaid.R;
@@ -13,7 +14,9 @@ public class Terms extends Activity {
 	// Logger constant
 	private static final String TAG = "Terms";
 	TextView txt;
-    /** 
+	String terms;
+	   
+	/** 
      * Method called when activity is created
      */
 	@Override
@@ -26,13 +29,34 @@ public class Terms extends Activity {
         
         txt = (TextView)findViewById(R.id.TextView01);
         try {
-			txt.setText(InternetUtils.getTOC("http://dev.floodaid.com.au/api/toc?api_key=abcdefg12345"));
+			loadTermsViaAPI();
 		} catch (Exception e) {
 			//as a backup when API is not reachable
 			txt.setText(R.string.termsOfUse);
 		}
         
-        
-        //TODO: Build menu items dynamically and add links to related activities
 	}
+	
+	protected void loadTermsViaAPI() 
+	{                
+		Thread t = new Thread() 
+		{            
+			public void run() 
+			{                
+				terms = InternetUtils.getTOC("http://dev.floodaid.com.au/api/toc?api_key=abcdefg12345");                
+				handler.post(setTerms);            
+			}        
+		};        
+		t.start();    
+	} 
+	
+	final Handler handler = new Handler();        
+	final Runnable setTerms = new Runnable() 
+	{        
+		public void run() 
+		{            
+			txt.setText(terms);        
+		}    
+	};
+    
 }
